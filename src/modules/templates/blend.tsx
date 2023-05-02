6d4646d023680bc2827159fe6f88404fd47babc7
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-05-02 22:56:02 +0100

    modify blend template page

--- src/modules/templates/blend.tsx
+++ src/modules/templates/blend.tsx
@@ -1,9 +1,7 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { auth, db } from "../../library";
 import { ExperienceList } from "../experience/components/listitem";
-import { ICV } from "../buildcv/model";
 import { SkillList } from "../skill/components/listitem";
 import { AcademyList } from "../academic/components/listitem";
 import { ApButton, ApLoader, ApModal } from "../../components";
@@ -14,40 +12,25 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import Pdf from "react-to-pdf";
 import { FiDownload } from "react-icons/fi";
+import { getCookie } from "../../services/helper";
 
 export const BlendTemplate = () => {
   const ref = React.createRef<any>();
-  const { getImageFile, imageFile } = useCvState();
-  const [cvState, setCvState] = useState<ICV>({} as any);
+  const { getImageFile, getCVDocument, loading, cvState } = useCvState();
   const [modal, setModal] = useState<{ show: boolean; data?: any }>({
     show: false,
   });
-  const [loading, setLoading] = useState<boolean>(false);
-  const [session, setSession] = useState({});
   const router = useRouter();
-  const getCVDocument = () => {
-    onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        setSession(user?.uid);
-        setLoading(true);
-        const cvDocRef = doc(db, "cv", user.uid);
-        const cvSnapShot = await getDoc(cvDocRef);
-        if (cvSnapShot.exists()) {
-          setLoading(false);
-          setCvState(cvSnapShot.data() as any);
-        }
-      }
-    });
-  };
+  const id = getCookie("user_id");
 
   useEffect(() => {
-    getCVDocument();
+    getCVDocument(id);
     getImageFile();
   }, []);
   return (
     <>
       <div>
-        {session ? (
+        {id ? (
           <div>
             {loading && (
               <div className="w-[100vw] h-[100vh] m-auto flex justify-center items-center">
@@ -61,47 +44,59 @@ export const BlendTemplate = () => {
               <div>
                 <div ref={ref} className="m-auto flex lg:w-[70%] h-auto">
                   <div className="lg:w-[40%] w-[30%] px-2 pt-4 lg:pt-0 bg-blend-darken bg-red-900 text-white">
-                    <div className="relative">
-                      <img
-                        src={imageFile}
-                        alt="name"
-                        className=" border w-[5rem] h-[5rem] rounded-full text-center m-auto lg:rounded-full"
-                      />
-                    </div>
+                    {cvState?.personalInformation?.image && (
+                      <div className="relative">
+                        <img
+                          src={cvState?.personalInformation?.image?.uri}
+                          alt={cvState?.personalInformation?.image?.name}
+                          className=" border w-[5rem] h-[5rem] rounded-full text-center m-auto lg:rounded-full"
+                        />
+                      </div>
+                    )}
                     <div className="pt-8 pb-4 text-2xl uppercase font-bold">
-                      {`${cvState?.firstName
+                      {`${cvState?.personalInformation?.firstName
                         ?.charAt(0)
-                        ?.toLocaleUpperCase()}${cvState?.firstName?.slice(
+                        ?.toLocaleUpperCase()}${cvState?.personalInformation?.firstName?.slice(
                         1
-                      )} ${cvState?.lastName
+                      )} ${cvState?.personalInformation?.lastName
                         ?.charAt(0)
-                        ?.toLocaleUpperCase()}${cvState?.lastName?.slice(1)}`}
+                        ?.toLocaleUpperCase()}${cvState?.personalInformation?.lastName?.slice(
+                        1
+                      )}`}
                     </div>
-                    <p className="">{cvState?.profession}</p>
+                    <p className="">
+                      {cvState?.personalInformation?.profession}
+                    </p>
 
                     <div>
-                      <p className="py-2">{cvState?.email}</p>
-                      <p className="py-2">{cvState?.phoneNumber}</p>
+                      <p className="py-2">
+                        {cvState?.personalInformation?.email}
+                      </p>
+                      <p className="py-2">
+                        {cvState?.personalInformation?.phoneNumber}
+                      </p>
                       <div className="">
                         {/* <HiOutlineLocationMarker size={20} /> */}
-                        <p className="">{cvState?.address}</p>
+                        <p className="">
+                          {cvState?.personalInformation?.address}
+                        </p>
                       </div>
                     </div>
                   </div>
                   <div className=" bg-gray-200 h-auto pt-4 lg:pt-0">
-                    {cvState.description ? (
+                    {cvState?.personalInformation?.description ? (
                       <div>
                         {" "}
                         <p className="mx-4 font-bold text-xl border border-b-gray-300">
                           DESCRIPTION
                         </p>
                         <p className="p-2 px-4 text-justify">
-                          {cvState?.description}
+                          {cvState?.personalInformation?.description}
                         </p>
                       </div>
                     ) : null}
                     <div className="w-full  p-4">
-                      {cvState.experience ? (
+                      {cvState?.experience ? (
                         <div className="">
                           <h1 className="uppercase font-bold mb-2 text-xl border border-b-gray-300">
                             Work History
@@ -114,7 +109,7 @@ export const BlendTemplate = () => {
                         </div>
                       ) : null}
                       <div>
-                        {cvState.skill ? (
+                        {cvState?.skill ? (
                           <div>
                             <h1 className="uppercase font-bold text-xl border border-b-gray-300 mb-2">
                               Skills
@@ -126,19 +121,19 @@ export const BlendTemplate = () => {
                             </div>
                           </div>
                         ) : null}
-                        {cvState.academy ? (
+                        {cvState?.academic ? (
                           <div>
                             <h1 className="font-bold text-xl uppercase border border-b-gray-300 mb-2">
                               Education
                             </h1>
                             <div className="my-4">
-                              {cvState?.academy?.map((a, i) => (
+                              {cvState?.academic?.map((a, i) => (
                                 <AcademyList academy={a} key={i} />
                               ))}
                             </div>
                           </div>
                         ) : null}
-                        {cvState.certificate ? (
+                        {cvState?.certificate ? (
                           <div>
                             <h1 className="font-bold text-lg uppercase border border-b-gray-300 mb-2">
                               Certification
